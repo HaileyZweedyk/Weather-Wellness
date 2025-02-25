fae9b5c9998d141f92066c26eb0b27fc4eb6682f
samanthaboeve <boevesa@mail.gvsu.edu>
2025-02-25 18:22:51 -0500

    skeleton tests

--- UnitTest.py
+++ UnitTest.py
@@ -1,1 +1,29 @@
-# This file is for unit tests
+import unittest
+import Weather from Weather
+import Wellness from Wellness
+
+class TestWeatherCodeTranslations(unittest.TestCase):
+    def test_get_conditions(self):
+        pass
+    def test_get_category(self):
+        pass
+
+class TestWeather(unittest.TestCase):
+    def test_forcast_daily(self):
+        pass
+    def test_forecast_hourly(self):
+        pass
+    def test_view_current_weather(self):
+        pass
+    def test_set_current_location(self):
+        pass
+
+class TestWellness(unittest.TestCase):
+    def test_suggest_clothing(self, temp):
+        pass
+    def test_suggest_activities(self,weather_conditions):
+        pass
+    def test_driving_techniques(self,weather_conditions):
+        pass
+    def test_mood_weather(self):
+        pass
